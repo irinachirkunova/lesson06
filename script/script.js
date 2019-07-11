b40cf13e449dea4c1ff42800c3fbf718757fc618
Irina Chirkunova <irina.chirkunova86@gmail.com>
2019-07-11 20:44:01 +0200

    creation lesson07 budget app

---
+++ script/script.js
@@ -0,0 +1,51 @@
+let button = document.getElementById('start'),
+
+    buttonWithTag = document.getElementsByTagName('button'),
+    buttonPlusOne = buttonWithTag[0],
+    buttonPlusTwo = buttonWithTag[1],
+
+    checkBox = document.querySelector('#deposit-check'),
+
+    inputsAddIncome = document.querySelectorAll('.additional_income-item'),
+    inputAddIncomeOne = inputsAddIncome[0],
+    inputAddIncomeTwo = inputsAddIncome[1],
+
+    inputsByClass = document.getElementsByClassName('result-total'),
+    inputMonth = inputsByClass[0],
+    inputDay = inputsByClass[1],
+    inputExpMonth = inputsByClass[2],
+    inputAddIncome = inputsByClass[3],
+    inputAddExp = inputsByClass[4],
+    inputIncPeriod = inputsByClass[5],
+    inputTotalMonth = inputsByClass[6],
+
+    all = document.querySelectorAll('input'),
+    salary = all[0],
+    incomeTitle = all[1],
+    incomeSum = all[2],
+    expTitle = all[5],
+    expAmount = all[6],
+    addExp = all[7],
+    target = all[11];
+   
+console.log(button);
+console.log(buttonPlusOne);
+console.log(buttonPlusTwo);
+console.log(checkBox);
+console.log(inputAddIncomeOne);
+console.log(inputAddIncomeTwo);
+console.log(inputsByClass);
+console.log(inputMonth);
+console.log(inputDay);
+console.log(inputExpMonth);
+console.log(inputAddIncome);
+console.log(inputAddExp);
+console.log(inputIncPeriod);
+console.log(inputTotalMonth);
+console.log(salary);
+console.log(incomeTitle);
+console.log(incomeSum)
+console.log(expTitle);
+console.log(expAmount);
+console.log(addExp)
+console.log(target);
