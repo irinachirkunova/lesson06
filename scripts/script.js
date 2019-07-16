265998f724dc81a9e69fab69fa5c279996c510c8
Irina Chirkunova <irina.chirkunova86@gmail.com>
2019-07-16 21:37:29 +0200

    creating lesson10 dif

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,64 @@
 'use strict'
-
-function DomElement(selector, height, width, bg, fontSize) {
-    this.selector = selector;
-    this.height = height;
-    this.width = width;
-    this.bg = bg;
-    this.fontSize = fontSize;
-    this.createElement = function (selector) {
-        let newElem;
-        if(selector === "."){
-            newElem = document.createElement('div');
-            newElem.textContent = 'DIV DIV DIV';
-            newElem.style.cssText =  `height:${height}px; width:${width}px; background: ${bg}; font-size: ${fontSize}px;`  
-        }
-        else if(selector === "#"){
-            newElem = document.createElement('p');
-            newElem.textContent  = 'P P P P P';
-            newElem.style.cssText =  `height:${height}px; width:${width}px; background: ${bg}; font-size: ${fontSize}px;`           
-        }     
-        document.body.appendChild(newElem);            
-    }
-}
  
+document.addEventListener('DOMContentLoaded', function() {
+    function DomElement(selector, height, width, bg, fontSize) {
+        this.selector = selector;
+        this.height = height;
+        this.width = width;
+        this.bg = bg;
+        this.fontSize = fontSize;
+        this.createElement = function (selector) {
+            let newElem;
+            if(selector === "."){
+                newElem = document.createElement('div');
+                newElem.style.cssText =  `height:${height}px; width:${width}px; background: ${bg}; font-size: ${fontSize}px;`  
+                newElem.style.position = 'absolute';
+            }
+            else if(selector === "#"){
+                newElem = document.createElement('p');
+                newElem.textContent  = 'P P P P P';
+                newElem.style.cssText =  `height:${height}px; width:${width}px; background: ${bg}; font-size: ${fontSize}px;`           
+            }     
+            document.body.appendChild(newElem);            
+        }
+    }
 
-let newElementOne = new DomElement('.', '200', '200', 'yellow', '50');
-newElementOne.createElement('.');
+    let newElementOne = new DomElement('.', '100', '100', 'green', '0');
+    newElementOne.createElement('.');
 
-let newElementTwo = new DomElement('#', '500', '500', 'pink', '50');
-newElementTwo.createElement('#');
+    let square = document.querySelector('div');
+  
+    document.addEventListener('keydown', changeKey);
+   
+    function changeKey(e) {
+        switch(e.keyCode) {
+            case 37: square.style.marginLeft = '10px'; // left
+            console.log(e.keyCode); 
+            break;
+
+            case 38: square.style.marginTop = '10px';// up
+            console.log(e.keyCode);
+            break;
+
+            case 39: square.style.marginLeft = '-10px'; // right
+            console.log(e.keyCode);
+            break;
+
+            case 40: square.style.marginTop = '-10px'; // down
+            console.log(e.keyCode);
+            break;
+
+            default: return; 
+        
+        }
+    };
+
+}) 
+
+
+
+
+
+
 
 
